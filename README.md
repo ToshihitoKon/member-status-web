a4d86a4bfda3e8addbd3ffbbae3ac3a903d16d06
Toshihito Kon <toshihito.kon@gmail.com>
2022-03-30 02:10:09 +0900

    基本操作はできるようになった

---
+++ README.md
@@ -0,0 +1,19 @@
+# member-status-web
+
+ページを開いている人の状態を共有するのツール
+
+## note
+
+利用者が打ち込む情報
+
+- room id
+- username
+- status
+- comment(optional)
+- icon url(optional)
+  - メアド入れたらgravaterから画像引っ張ってくるみたいな夢もある
+
+ページ構成
+
+- top
+- room
--- src/main.ts
+++ src/main.ts
@@ -7,21 +7,32 @@ const server: any = http.createServer(app)
 const io: any = new Server(server)
 
 app.get('/', (req:any, res:any)=>{
+    res.sendFile(__dirname + '/static/top.html')
+})
+app.get('/room', (req:any, res:any)=>{
     res.sendFile(__dirname + '/static/index.html')
 })
 
 io.on('connection', (socket: any)=>{
-    console.log('a user connected')
+    console.log('client connected')
 
     // disconnect
     socket.on('disconnect', ()=>{
         console.log('user disconnected')
+        io.emit('leave', {
+            'socket_id': socket.id
+        })
     })
+
     // get message
-    socket.on('chat message', (msg: any)=>{
-        console.log('message: ' + msg)
-        // socket.broadcast.emit(msg)
-        io.emit('chat message', msg)
+    socket.on('user_status', (msg: any)=>{
+        msg['socket_id'] = socket.id
+        console.log(msg)
+        io.emit('user_status', msg)
+    })
+    socket.on('request_status', (msg: any)=>{
+        console.log(msg)
+        io.emit('request_status', msg)
     })
 })
 
